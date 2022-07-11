4627ab2247c91a73412e09e5e2c4be9352e5112d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-07-11 13:23:30 +0530

    Commit after alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+const [mode, setMode] = useState('light')
+const [alert, setAlert] = useState('light')
+
+const showAlert = (message,type) => {
+  setAlert({
+    msg: message,
+    type: type
+  })
+}
+
+const toggleMode = () => {
+  if(mode === 'light') {
+    setMode('dark')
+    document.body.style.backgroundColor = '#042743'
+    setAlert()
+  }
+  else {
+    setMode('light')
+    document.body.style.backgroundColor = 'white'
+    setAlert()
+  }
+}
+
   return (
     <>
-      <Navbar title='TetxtUtils' aboutText='About TextUtils'/>
+      <Navbar title='TetxtUtils' aboutText='About TextUtils' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={`This is ${alert} mode`} />
       <div className='container'>
-        <TextForm heading='Enter the text to analyze'/>
+        <TextForm heading='Enter the text to analyze' mode={mode}/>
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,9 +2,38 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        // console.log('Uppercas was clicked' , text) 
+        // Converts to Uppercase
         let newText = text.toUpperCase()
         setText(newText)
+    }
+
+    const handleLoClick = () => {
+        // Converts to Lowercase
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        // Clears the text 
+        let newText = ''
+        setText(newText)
+    }
+
+    const handleCamelClick = () => {
+        // camelCaseTheText 
+        let newText = text.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+        setText(newText)
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById('myBox')
+        text.select();
+        navigator.clipboard.writeText(text.value)
+    }
+
+    const handleSpaces = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(' '))
     }
 
     const handleOnChange = (event) => {
@@ -20,20 +49,27 @@ export default function TextForm(props) {
         }
     }
 
-    const [text,setText] = useState('Enter text Here')
+    const [text,setText] = useState('')
   return (
     <>
-    <div className='container'>
-        <h1>{props.heading}</h1>
+    <div className='container' style={{color: props.mode === 'light'? 'black': 'white'}}>
+        <h1 className='my-3'>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+        <textarea className="form-control" style={{backgroundColor: props.mode === 'light'? 'white': 'grey',color: props.mode === 'light'? 'black': 'white'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleCamelClick}>CamelCase Text</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleSpaces}>Remove Extra Spaces</button>
         </div>
     </div>
-    <div className="container">
-        <h1>Your text summary</h1>
+    <div className="container" style={{color: props.mode === 'light'? 'black': 'white'}}>
+        <h2>Your text summary</h2>
         <p>{textLength(text)} words and {text.trim().length} characters</p>
         <p>{textLength(text) * 0.008} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   )
