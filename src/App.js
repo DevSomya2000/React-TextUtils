02bc915c1ab42b85edb3ded136f1770b84addd3b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-07-12 11:48:06 +0530

    Wrapping TextUtils

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const toggleMode = () => {
   if(mode === 'light') {
     setMode('dark')
     document.body.style.backgroundColor = '#042743'
-    document.title = "TextUtils - Dark Mode"
+    // document.title = "TextUtils - Dark Mode"
     showAlert('Dark mode has been enabled', 'success')
     // setInterval(() => {
     //   document.title = "TextUtils is Amazing Now"
@@ -41,7 +41,7 @@ const toggleMode = () => {
   else {
     setMode('light')
     document.body.style.backgroundColor = 'white'
-    document.title = "TextUtils - Light Mode"
+    // document.title = "TextUtils - Light Mode"
     showAlert('Light mode has been enabled', 'success')
   }
 }
@@ -49,12 +49,13 @@ const toggleMode = () => {
   return (
     <>
       <Router>
-        <Navbar title='TetxtUtils' aboutText='About TextUtils' mode={mode} toggleMode={toggleMode} />
+        <Navbar title='TetxtUtils' aboutText='About' mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className='container'>
           <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading='Enter the text to analyze' mode={mode} />} />
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter, Removes Extra Spaces'
+             mode={mode} />} />
           </Routes>
         </div>
       </Router>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,8 +30,6 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById('myBox')
-        text.select();
         navigator.clipboard.writeText(text.value)
         props.showAlert('Copied the text', 'success')
     }
@@ -46,38 +44,38 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const textLength = (text) => {
-        if(text==='') {
-            return 0
-        }
-        else {
-            let newText = text.replace('\n',' ').trim().split(/[ ]+/).join(' ')
-            console.log(newText)
-            return newText.split(' ').length
-        }
-    }
+    // const textLength = (text) => {
+    //     if(text==='') {
+    //         return 0
+    //     }
+    //     else {
+    //         let newText = text.replace('\n',' ').trim().split(/[ ]+/).join(' ')
+    //         console.log(newText)
+    //         return newText.split(' ').length
+    //     }
+    // }
 
     const [text,setText] = useState('')
   return (
     <>
-    <div className='container' style={{color: props.mode === 'light'? 'black': 'white'}}>
-        <h1 className='my-3'>{props.heading}</h1>
+    <div className='container' style={{color: props.mode === 'light'? '#042743': 'white'}}>
+        <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" style={{backgroundColor: props.mode === 'light'? 'white': 'grey',color: props.mode === 'light'? 'black': 'white'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleClearClick}>Clear Text</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleCamelClick}>CamelCase Text</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={handleSpaces}>Remove Extra Spaces</button>
+        <textarea className="form-control" style={{backgroundColor: props.mode === 'light'? 'white': '#13466e',color: props.mode === 'light'? '#042743': 'white'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleCamelClick}>CamelCase Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleCopy}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 mx-2" onClick={handleSpaces}>Remove Extra Spaces</button>
         </div>
     </div>
     <div className="container" style={{color: props.mode === 'light'? 'black': 'white'}}>
         <h2>Your text summary</h2>
-        <p>{textLength(text)} words and {text.trim().length} characters</p>
-        <p>{textLength(text) * 0.008} Minutes read</p>
+        <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.trim().length} characters</p>
+        <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length * 0.008} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0? text:"Nothing to preview"}</p>
     </div>
     </>
   )
